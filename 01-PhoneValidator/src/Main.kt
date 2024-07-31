30e6d396c9211385cdd5972fc96c076cdad13457
sedrac-slc <slcsedrac@gmail.com>
2024-07-31 20:23:11 +0100

    modfliy regex validate format number

--- 01-PhoneValidator/src/Main.kt
+++ 01-PhoneValidator/src/Main.kt
@@ -1,3 +1,3 @@
-fun numberValid(number: String): Boolean = "9[1235][0-9]{7}|(999|900|909)[0-9]{6}".toRegex() matches number
+fun numberValid(number: String): Boolean = "9[12345][0-9]{7}|99[1-8][0-9]{6}".toRegex() matches number
 
-fun main() = println(numberValid("909269780"))
+fun main() = println(numberValid("998269780"))
